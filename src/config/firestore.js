d50b3a1acb1aca5a19032ebb909096c93f8cd66b
Siyu Liu <siyu.java.web@gmail.com>
2023-09-28 14:46:35 +1000

    installs firestore and adds configs

---
+++ src/config/firestore.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDBSjM1Ts0E82B5n8ske3NVX_Diuf3y3w4",
+  authDomain: "e-commerce-siyu.firebaseapp.com",
+  projectId: "e-commerce-siyu",
+  storageBucket: "e-commerce-siyu.appspot.com",
+  messagingSenderId: "170705461351",
+  appId: "1:170705461351:web:17a51d19cf8085ce6d74c2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
