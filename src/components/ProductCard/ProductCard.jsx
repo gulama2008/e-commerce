a0212add4bc2f63d3e5820102f5571eadc3e269b
Siyu Liu <siyu.java.web@gmail.com>
2023-09-30 16:59:52 +1000

    adds styles to products page

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import styles from './ProductCard.module.scss'
+import { Link, NavLink } from 'react-router-dom';
 const ProductCard = ({ product }) => {
   console.log(product);
   return (
-    <div>ProductCard</div>
+    <Link to={`${product.id}`} className={ styles.card}>
+      <img className={ styles.img} src={ product.image} alt="" />
+      <h3>${ product.price}</h3>
+      <p>{product.name}</p>
+    </Link>
   )
 }
 
