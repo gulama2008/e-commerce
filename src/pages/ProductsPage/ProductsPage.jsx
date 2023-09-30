c76034cacf9030a45488297fd136eb34c285d794
Siyu Liu <siyu.java.web@gmail.com>
2023-09-30 11:39:26 +1000

    adds products page and product card

--- src/pages/ProductsPage/ProductsPage.jsx
+++ src/pages/ProductsPage/ProductsPage.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ProductsList from '../../containers/ProductsPageLoader/ProductsPageLoader'
+import ProductCard from '../../components/ProductCard/ProductCard';
 
-const ProductsPage = ({ products}) => {
+const ProductsPage = ({ products,category}) => {
   console.log(products);
   return (
     <div>
-      <h2>Product Page</h2>
-      {/* <ProductsList /> */}
+      <h2>{ category}</h2>
+      <ul>
+        {products.map(product => { 
+          return <li key={product.id}><ProductCard product={ product} /></li>
+        })}
+      </ul>
     </div>
   );
 }
