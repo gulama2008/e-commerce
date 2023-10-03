7f48c8c2f71ee0936dc4f59f9edd193d7159e9b8
Siyu Liu <siyu.java.web@gmail.com>
2023-10-03 19:47:34 +1100

    adds styles to product page

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,10 +1,15 @@
-import React, { useContext, useState } from 'react'
-import styles from './ProductPage.module.scss'
-import { ProductsContext } from '../../context/ProductsContextProvider';
-import { saveItemsToSessionStorage, toFloat } from '../../services/data-service';
-import { NavLink } from 'react-router-dom';
-const ProductPage = ({ product,category }) => {
-  const { itemsInCart,updateCart } = useContext(ProductsContext);
+import React, { useContext, useState } from "react";
+import styles from "./ProductPage.module.scss";
+import { ProductsContext } from "../../context/ProductsContextProvider";
+import {
+  saveItemsToSessionStorage,
+  toFloat,
+} from "../../services/data-service";
+import { NavLink } from "react-router-dom";
+import QuantityButton from "../../components/QuantityButton/QuantityButton";
+const ProductPage = ({ product, category }) => {
+  const { itemsInCart, updateCart, changeItemQuantityInCart } =
+    useContext(ProductsContext);
   const [activeSizeIndex, setActiveSizeIndex] = useState(0);
   const [quantity, setQuantity] = useState(product.quantity[0]);
   const [quantityAdd, setQuantityAdd] = useState(1);
@@ -13,40 +18,45 @@ const ProductPage = ({ product,category }) => {
   console.log(product);
   console.log(activeSizeIndex);
   console.log(product.size[activeSizeIndex]);
-  const onClickSizeButton = (e) => { 
+  const onClickSizeButton = (e) => {
     setQuantityAdd(1);
-    const index = e.target.getAttribute('index');
+    const index = e.target.getAttribute("index");
     setActiveSizeIndex(index);
     setQuantity(product.quantity[index]);
-    
-  }
+  };
   const changeQuantityPurchase = (e) => {
-    setQuantityAdd(parseInt(e.target.value))
-  }
-  const addToCart = () => { 
+    setQuantityAdd(parseInt(e.target.value));
+  };
+  const increaseQuantity = (e) => {
+    setQuantityAdd(quantityAdd+1)
+  };
+  const decreaseQuantity = (e) => {
+    setQuantityAdd(quantityAdd-1)
+  };
+  const addToCart = () => {
     // setIsAddedToCart(true);
     // setQuantity(quantity - parseInt(quantityAdd));
-    
+
     setQuantityInCart(quantityInCart + parseInt(quantityAdd));
     const newItem = {
       ...product,
       size: product.size[activeSizeIndex],
       quantity: quantityAdd,
-      stock: quantity
+      stock: quantity,
     };
     console.log(newItem);
     // saveItemsToSessionStorage(newItem);
     updateCart(newItem);
     setQuantityAdd(1);
-  }
+  };
   console.log(itemsInCart);
   return (
     <div>
       <div className={styles.container}>
         <img src={product.image} alt="" className={styles.img} />
         <div className={styles.info}>
-          <p>{product.name}</p>
-          <p>${toFloat(product.price)}</p>
+          <p className={styles.info_name}>{product.name}</p>
+          <p className={styles.info_price}>${toFloat(product.price)}</p>
           <div>
             {product.size.map((size, index) => {
               return (
@@ -61,23 +71,40 @@ const ProductPage = ({ product,category }) => {
               );
             })}
           </div>
-          <p>In stock: {quantity} left</p>
-          <input
-            type="number"
-            min={1}
-            max={quantity}
-            value={quantity == 0 ? 0 : quantityAdd}
-            onChange={changeQuantityPurchase}
-          />
-          {quantity == 0 && <p>Sorry, no stock left</p>}
-          <button onClick={addToCart} disabled={ quantity==0}>Add to Cart</button>
+          <p className={styles.info_stock}>In stock: {quantity} left</p>
+          <div className={styles.quantity}>
+            <button
+              className={styles.quantity_btn}
+              onClick={decreaseQuantity}
+              disabled={quantityAdd == 1}
+            >
+              -
+            </button>
+            <input
+              type="text"
+              className={styles.quantity_input}
+              value={ quantityAdd}
+              onChange={changeQuantityPurchase}
+            />
+            <button
+              className={styles.quantity_btn}
+              onClick={increaseQuantity}
+              disabled={quantityAdd == quantity}
+            >
+              +
+            </button>
+            {quantity == 0 && <p>Sorry, no stock left</p>}
+          </div>
+          <button onClick={addToCart} disabled={quantity == 0} className={ styles.add}>
+            Add to Cart
+          </button>
         </div>
       </div>
       <NavLink to={`/${category}`}>Back</NavLink>
     </div>
   );
-}
+};
 
-export default ProductPage
+export default ProductPage;
 
 // className={ activeSizeIndex==index?`{styles.btn}`:`{styles.btn} {styles.btn_active}`}>
