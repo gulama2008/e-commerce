5d0b0d4afa967491a7ffaf456740d9602665bbfd
Siyu Liu <siyu.java.web@gmail.com>
2023-10-07 12:20:48 +1100

    adds funtion of adding or deducting stock when clicking quantity button in cart page

--- src/components/QuantityButton/QuantityButton.jsx
+++ src/components/QuantityButton/QuantityButton.jsx
@@ -1,8 +1,9 @@
 import React, { useContext, useState } from 'react'
 import styles from './QuantityButton.module.scss'
 import { ProductsContext } from '../../context/ProductsContextProvider';
+import { updateStock } from '../../services/products-service';
 const QuantityButton = ({ item,index, setQuantityInCart,quantityInCart}) => {
-  const { changeItemQuantityInCart } =
+  const { products, changeItemQuantityInCart } =
     useContext(ProductsContext);
   const [quantity, setQuantity] = useState(quantityInCart);
   const changeQuantity = (e) => {
@@ -11,11 +12,25 @@ const QuantityButton = ({ item,index, setQuantityInCart,quantityInCart}) => {
     changeItemQuantityInCart(index, parseInt(e.target.value));
   };
   const increaseQuantity = (e) => {
+    const itemToBeDeductFromStock = products.find((product) => product.id == item.id);
+    const sizeIndex = itemToBeDeductFromStock.size.findIndex(
+      (size) => size == item.size
+    );
+    itemToBeDeductFromStock.quantity[sizeIndex] -= 1;
+    updateStock(itemToBeDeductFromStock.id, itemToBeDeductFromStock.quantity);
     setQuantity(quantityInCart + 1);
     setQuantityInCart(quantityInCart + 1);
     changeItemQuantityInCart(index, quantityInCart+1);
   };
   const decreaseQuantity = (e) => {
+    const itemToBeAddToStock = products.find(
+      (product) => product.id == item.id
+    );
+    const sizeIndex = itemToBeAddToStock.size.findIndex(
+      (size) => size == item.size
+    );
+    itemToBeAddToStock.quantity[sizeIndex] += 1;
+    updateStock(itemToBeAddToStock.id, itemToBeAddToStock.quantity);
     setQuantity(quantityInCart - 1);
     setQuantityInCart(quantityInCart - 1);
     changeItemQuantityInCart(index, quantityInCart - 1);
