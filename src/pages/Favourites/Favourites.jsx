08a3421cb1a093128952e1c8f8f413a0a83b9b5f
Siyu Liu <siyu.java.web@gmail.com>
2023-10-08 15:09:08 +1100

    adds functionality of showing no favourited items text

--- src/pages/Favourites/Favourites.jsx
+++ src/pages/Favourites/Favourites.jsx
@@ -2,27 +2,41 @@ import React, { useEffect, useState } from "react";
 import ProductCard from "../../components/ProductCard/ProductCard";
 import styles from "./Favourites.module.scss";
 import { NavLink } from "react-router-dom";
-import { subscribeToProducts } from '../../services/products-service';
+import { subscribeToProducts } from "../../services/products-service";
 const Favourites = () => {
-    const [products, setProducts] = useState([]);
-    useEffect(() => {
-        const unsub = subscribeToProducts(setProducts);
-      return () => unsub();
-    }, []);
+  const [products, setProducts] = useState([]);
+  const [hasFavouritedItems, setHasFavouritedItems] = useState(false);
+console.log('render');
+  useEffect(() => {
+    if (products.filter((product) => product.isFavourited).length!==0) {
+      setHasFavouritedItems(true);
+    } else {
+      setHasFavouritedItems(false);
+    }
+  }, [products]);
+
+  useEffect(() => {
+    const unsub = subscribeToProducts(setProducts);
+    return () => unsub();
+  }, []);
 
   return (
     <div className={styles.container}>
       <div className={styles.heading}>
-        <h2>
-          My Favourites
-        </h2>
+        <h2>My Favourites</h2>
       </div>
 
-      <div className={styles.products_list}>
-              {products.filter(product => product.isFavourited).map((product) => {
-          return <ProductCard product={product} key={product.id} />;
-        })}
-      </div>
+      {hasFavouritedItems ? (
+        <div className={styles.products_list}>
+          {products
+            .filter((product) => product.isFavourited)
+            .map((product) => {
+              return <ProductCard product={product} key={product.id} />;
+            })}
+        </div>
+      ) : (
+        <div>You have no favourited items</div>
+      )}
       <NavLink to="/">
         <button className={styles.btn}>Back</button>
       </NavLink>
@@ -30,4 +44,4 @@ const Favourites = () => {
   );
 };
 
-export default Favourites
+export default Favourites;
